aa132b31a8a22cc24797cb0eccb7ba088aa210c6
unknown <529042309@qq.com>
2019-11-14 18:03:47 +0800

    xpp back

--- Second/src/common/src/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as BasicLayout } from './components/BasicLayout';
-export { default as SystemsEntry } from './components/SystemsEntry';
-export { default as UserMenu } from './components/UserMenu';
-export { default as CardWithTitle } from './components/CardWithTitle';
